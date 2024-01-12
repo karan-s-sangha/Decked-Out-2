d9cc4ed8aa02d71a9411570b090413509c6bf3c1
Khin_Win <kwin2@uw.edu>
2024-01-12 11:19:05 -0800

    merge update one to Khin

---
+++ Main.js
@@ -0,0 +1,44 @@
+
+var ASSET_MANAGER = new AssetManager();
+
+// spritesheets
+ASSET_MANAGER.queueDownload("./Level_1_LowerView.png");
+ASSET_MANAGER.queueDownload("./Level_1_MiddleView.png");
+ASSET_MANAGER.queueDownload("./Level_1_UpperView.png");
+
+
+// // music
+// ASSET_MANAGER.queueDownload("./music/overworld.mp3");
+// ASSET_MANAGER.queueDownload("./music/underworld.mp3");
+// ASSET_MANAGER.queueDownload("./music/overworld-hurry.mp3");
+// ASSET_MANAGER.queueDownload("./music/underworld-hurry.mp3");
+
+// // sound effects
+// ASSET_MANAGER.queueDownload("./audio/small-jump.mp3");
+// ASSET_MANAGER.queueDownload("./audio/super-jump.mp3");
+// ASSET_MANAGER.queueDownload("./audio/stomp.mp3");
+// ASSET_MANAGER.queueDownload("./audio/block.mp3");
+// ASSET_MANAGER.queueDownload("./audio/bump.wav");
+
+ASSET_MANAGER.downloadAll(function () {
+	var gameEngine = new GameEngine();
+
+	// ASSET_MANAGER.autoRepeat("./music/overworld.mp3");
+	// ASSET_MANAGER.autoRepeat("./music/underworld.mp3");
+	// ASSET_MANAGER.autoRepeat("./music/overworld-hurry.mp3");
+	// ASSET_MANAGER.autoRepeat("./music/underworld-hurry.mp3");
+
+	//PARAMS.BLOCKWIDTH = PARAMS.BITWIDTH * PARAMS.SCALE;  // used for scaling the map
+
+	var canvas = document.getElementById('gameWorld');
+	var ctx = canvas.getContext('2d');
+
+	//PARAMS.CANVAS_WIDTH = canvas.width;
+	//PARAMS.CANVAS_HEIGHT = canvas.height;
+
+	gameEngine.init(ctx);
+		
+	new SceneManager(gameEngine);
+
+	gameEngine.start();
+});
---
+++ SceneManager.js
@@ -0,0 +1,68 @@
+class SceneManager {
+    constructor(game) {
+        this.game = game; // game = GameEngine
+        this.ctx = game.ctx;
+        this.game.camera = this;
+        this.level = "./Level_1_UpperView.png";
+        this.scale = 1.5;
+        this.levelX=-1900;
+        this.levelY=0;
+    
+        this.menuButtonCooldown = 0.15;
+
+        //this.coinAnimation = new Animator(ASSET_MANAGER.getAsset("./sprites/coins.png"), 0, 160, 8, 8, 4, 0.2, 0, false, true);
+        this.loadLevel(this.level, 0, 0);
+        
+    };
+
+    clearEntities() {
+        this.game.entities.forEach(function (entity) {
+            entity.removeFromWorld = true;
+        });
+    };
+    
+    // loadLevel is supposed to have the introduction or title screen of the map
+
+    loadLevel(level, x, y) {
+        if(level == this.level){
+            let image = ASSET_MANAGER.cache[level];
+            this.ctx.drawImage(image,-1500,0, image.width*this.scale, image.height*this.scale);
+        }
+
+    };
+
+    updateAudio() {
+        var mute = document.getElementById("mute").checked;
+        var volume = document.getElementById("volume").value;
+
+        ASSET_MANAGER.muteAudio(mute);
+        ASSET_MANAGER.adjustVolume(volume);
+
+    };
+
+    update() {
+    
+        if(this.game.right){
+            this.levelX -=4;
+        }
+        if(this.game.left){
+            this.levelX +=4;
+        }
+        if(this.game.up){
+            this.levelY +=4;
+
+        }
+        if(this.game.down){
+            this.levelY -=4;
+
+        }
+
+        
+    };
+
+    draw(ctx) {
+        let image = ASSET_MANAGER.cache[this.level];
+        this.ctx.imageSmoothingEnabled = false;
+        this.ctx.drawImage(image,this.levelX,this.levelY, image.width*this.scale, image.height*this.scale);
+    };
+};
