051da87070fe6d9992a7b78eb779e3f16f66b8bd
Ingeun Hwang <ingeun@uw.edu>
2024-01-17 19:57:52 -0800

    Sprite rotation attemp #1

--- Steve.js
+++ Steve.js
@@ -7,7 +7,9 @@ class Steve {
         this.luigi = luigi;
         this.spritesheet = null;  // Placeholder for the image
         this.dir = "right";
-
+        this.cashe = [];
+        this.mousex = 0;
+        this.mousey = 0;
         this.loadAnimations();
     };
 
@@ -20,51 +22,126 @@ class Steve {
 
 
     update() {
-        if (this.game.left) {
-            this.dir = "left";
-            //this.x -= this.speed;
-        }
-        if (this.game.right) {
-            this.dir = "right";
-            //this.x += this.speed;
-        }
-        if (this.game.up) {
-            this.dir = "up";
-            //this.y += this.speed;
-        }
-        if (this.game.down) {
-            this.dir = "down";
-           // this.y -= this.speed;
-        }
+        // if (this.game.left) {
+        //     this.dir = "left";
+        //     //this.x -= this.speed;
+        // }
+        // if (this.game.right) {
+        //     this.dir = "right";
+        //     //this.x += this.speed;
+        // }
+        // if (this.game.up) {
+        //     this.dir = "up";
+        //     //this.y += this.speed;
+        // }
+        // if (this.game.down) {
+        //     this.dir = "down";
+        //    // this.y -= this.speed;
+        // }
 
     };
 
+    drawAngle(ctx, angle){
+        if(angle < 0 || angle > 359) {
+            return;
+        }
+
+        if(!this.cashe[angle]) {
+            let radian = angle / 360 * 2 * Math.PI;
+            var offscreenCanvas = document.createElement('canvas');
+
+            offscreenCanvas.width = 64;
+            offscreenCanvas.height = 64;
+
+            var offscreenCtx = offscreenCanvas.getContext('2d');
+            
+            offscreenCtx.save();
+            offscreenCtx.translate(12,32);
+            offscreenCtx.rotate(radian);
+            offscreenCtx.translate(-12,-32);
+            offscreenCtx.drawImage(this.spritesheet, 211, 0, 12, 16, 0, 13, 24,32);
+            offscreenCtx.restore();
+            this.cashe[angle] = offscreenCanvas;
+
+
+
+        //    var offscreenCanvas = document.createElement('canvas');
+        // offscreenCanvas.width = 64;
+        // offscreenCanvas.height = 64;
+        // var offscreenCtx = offscreenCanvas.getContext('2d');
+        // offscreenCtx.save();
+        // offscreenCtx.translate(32,32);
+        // offscreenCtx.rotate(Math.PI/1);
+        // offscreenCtx.translate(-32,-32);
+        // offscreenCtx.drawImage(this.spritesheet, 209, 0, 32, 16, 8, 0, 64,32);
+        // offscreenCtx.restore();
+
+        // ctx.drawImage(offscreenCanvas,this.x-16, this.y, 64, 64);
+
+
+        }
+        ctx.drawImage(this.cashe[angle],this.x + 4, this.y - 12);
+
+    }
 
     draw(ctx) {
-        // Working Code
-        if (this.game.left) {
-            ctx.save();
-            ctx.scale(-1, 1);
-            this.animations.drawFrame(this.game.clockTick, ctx, -this.x - 32, this.y, 2);
-            ctx.restore();
-        } else if (this.dir == "left") {
-            ctx.save();
-            ctx.scale(-1, 1);
-            ctx.drawImage(this.spritesheet, 209, 0, 32, 16, -this.x-32, this.y, 64,32);
-            ctx.restore();
-        }
-        else if (this.game.right) {
-            this.animations.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
+        // // Working Code
+        // if (this.game.left) {
+        //     ctx.save();
+        //     ctx.scale(-1, 1);
+        //     this.animations.drawFrame(this.game.clockTick, ctx, -this.x - 32, this.y, 2);
+        //     ctx.restore();
+        // } else if (this.dir == "left") {
+        //     ctx.save();
+        //     ctx.scale(-1, 1);
+        //     ctx.drawImage(this.spritesheet, 209, 0, 32, 16, -this.x-32, this.y, 64,32);
+        //     ctx.restore();
+        // }
+        // else if (this.game.right) {
+        //     this.animations.drawFrame(this.game.clockTick, ctx, this.x, this.y, 2);
             
-        } 
-        else if (this.dir == "right") {
-            ctx.drawImage(this.spritesheet, 209, 0, 32, 16, this.x , this.y, 64,32);
+        // } 
+        // else if (this.dir == "right") {
+        //     ctx.drawImage(this.spritesheet, 209, 0, 32, 16, this.x , this.y, 64,32);
+        // }
+        // if (this.game.up) {
+        //     // this.y -= this.speed;
+        // }
+        // if (this.game.down) {
+        //     // this.y += this.speed;
+        // }
+
+
+        ////////////////////////////////////////////////////////
+        // Rotation code
+        ////////////////////////////////////////////////////////
+
+        // var offscreenCanvas = document.createElement('canvas');
+        // offscreenCanvas.width = 64;
+        // offscreenCanvas.height = 64;
+        // var offscreenCtx = offscreenCanvas.getContext('2d');
+        // offscreenCtx.save();
+        // offscreenCtx.translate(32,32);
+        // offscreenCtx.rotate(Math.PI/1);
+        // offscreenCtx.translate(-32,-32);
+        // offscreenCtx.drawImage(this.spritesheet, 209, 0, 32, 16, 8, 0, 64,32);
+        // offscreenCtx.restore();
+
+        // ctx.drawImage(offscreenCanvas,this.x-16, this.y, 64, 64);
+
+
+        ////////////////////////////////////////////////////////
+        // Rotate 360
+        ////////////////////////////////////////////////////////
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(this.x, this.y, 32, 32);
+        ctx.save();
+        let angle = Math.atan2(this.game.mouse.y - this.y, this.game.mouse.x - this.x);
+        if(angle < 0) {
+            angle += Math.PI * 2;
         }
-        if (this.game.up) {
-            // this.y -= this.speed;
-        }
-        if (this.game.down) {
-            // this.y += this.speed;
-        }
+        let degrees = Math.floor(angle / Math.PI / 2 * 360);    
+        this.drawAngle(ctx, degrees);
+
     };
 };
