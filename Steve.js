74da5ff7dd47ad4c6202a7ef780521715f9caac0
Ingeun Hwang <ingeun@uw.edu>
2024-01-30 17:51:31 -0800

    working code

--- Steve.js
+++ Steve.js
@@ -7,21 +7,26 @@ class Steve {
         this.height = 340; 
         this.game = game;
         this.health = 10;
-        this.hunger = 0.5;
+        this.hunger = 10;
         this.steve = 10;
         this.spritesheet = null;  // Placeholder for the image
         this.move = 0;
         this.cache = [];
         this.mousex = 0;
         this.mousey = 0;
-        
+        this.run = false;
+        this.velocity = { z: 0 };
+        this.elapsedTime = 0;
+        this.jumped = false;
+        this.jumpComplete = true;
 
         this.playerX = playerX;
         this.playerY = playerY;
         this.screenX = this.game.ctx.canvas.width/2;
         this.screenY = this.game.ctx.canvas.height/2;
 
-        this.playerSpeed = 8;
+        this.playerRunSpeed = 10;
+        this.playerWalkSpeed = 6;
         this.collision = new Collision(this.game);
         this.loadAnimations();
     };
@@ -30,28 +35,55 @@ class Steve {
         this.spritesheet = new Image();
         this.spritesheet = ASSET_MANAGER.cache["./Art/Steve_Animations/player - running.png"];
         // this.spritesheet.src = "./Art/Steve_Animations/player - running.png";
-        this.animations = new Animator(this.game, this.spritesheet, 0, 0, this.width, this.height, 70, 0.008, 0, false, true);
+        this.walkingAnimations = new Animator(this.game, this.spritesheet, 0, 0, this.width, this.height, 70, 0.010, 0, false, true);
+        this.runningAnimations = new Animator(this.game, this.spritesheet, 0, 0, this.width, this.height, 70, 0.006, 0, false, true);
     };
 
 
 
     update() {
-        if (this.game.keys.left && !this.collision.isCollision(this.playerX - this.playerSpeed, this.playerY)) {
-            this.move = 1;
-            this.playerX -= this.playerSpeed;
-        } 
-        if (this.game.keys.right && !this.collision.isCollision(this.playerX + this.playerSpeed, this.playerY)) {
-            this.move = 1;
-            this.playerX += this.playerSpeed;
+       
+
+        if(this.game.keys.shift && this.hunger >= 3) {
+            if (this.game.keys.left && !this.collision.isCollision(this.playerX - this.playerRunSpeed, this.playerY)) {
+                this.move = 1;
+                this.playerX -= this.playerRunSpeed;
+                } 
+                if (this.game.keys.right && !this.collision.isCollision(this.playerX + this.playerRunSpeed, this.playerY)) {
+                    this.move = 1;
+                    this.playerX += this.playerRunSpeed;
+                }
+                if (this.game.keys.up && !this.collision.isCollision(this.playerX, this.playerY - this.playerRunSpeed)) {
+                    this.move = 1;
+                    this.playerY -= this.playerRunSpeed;
+                }
+                if (this.game.keys.down && !this.collision.isCollision(this.playerX, this.playerY + this.playerRunSpeed)) {
+                    this.move = 1;
+                    this.playerY += this.playerRunSpeed;
+                } 
+
+                this.run = true;
+        } else {
+            if (this.game.keys.left && !this.collision.isCollision(this.playerX - this.playerWalkSpeed, this.playerY)) {
+                this.move = 1;
+                this.playerX -= this.playerWalkSpeed;
+            } 
+            if (this.game.keys.right && !this.collision.isCollision(this.playerX + this.playerWalkSpeed, this.playerY)) {
+                this.move = 1;
+                this.playerX += this.playerWalkSpeed;
+            }
+            if (this.game.keys.up && !this.collision.isCollision(this.playerX, this.playerY - this.playerWalkSpeed)) {
+                this.move = 1;
+                this.playerY -= this.playerWalkSpeed;
+            }
+            if (this.game.keys.down && !this.collision.isCollision(this.playerX, this.playerY + this.playerWalkSpeed)) {
+                this.move = 1;
+                this.playerY += this.playerWalkSpeed;
+            } 
+
+            this.run = false;
         }
-        if (this.game.keys.up && !this.collision.isCollision(this.playerX, this.playerY - this.playerSpeed)) {
-            this.move = 1;
-            this.playerY -= this.playerSpeed;
-        }
-        if (this.game.keys.down && !this.collision.isCollision(this.playerX, this.playerY + this.playerSpeed)) {
-            this.move = 1;
-            this.playerY += this.playerSpeed;
-        } 
+
         if (!this.game.keys.left  && !this.game.keys.right && !this.game.keys.up && !this.game.keys.down){
             this.move = 0;
         }
@@ -106,7 +138,7 @@ class Steve {
         This is the reason we had to convert radian into degree. Further explanation can be
         seen from Chris's lecture.
         */
-        if(!this.cache[angle]) {
+    //    if(!this.cache[angle]) {
             let radian = angle / 360 * 2 * Math.PI;
             var offscreenCanvas = document.createElement('canvas');
 
@@ -132,11 +164,11 @@ class Steve {
             // offscreenCtx.strokeStyle="red";
             // offscreenCtx.strokeRect(0,0,offscreenCanvas.width, offscreenCanvas.height);
 
-            this.cache[angle] = offscreenCanvas;
+           // this.cache[angle] = offscreenCanvas;
 
-        }
+    //    }
         //ctx.drawImage(this.cache[angle],this.game.camera.cameraX - this.cache[angle].width / 2, this.game.camera.cameraY - this.cache[angle].height / 2);
-        ctx.drawImage(this.cache[angle],this.playerX - this.game.camera.cameraX - this.scale * this.height / 2, this.playerY - this.game.camera.cameraY - this.scale * this.height / 2);
+        ctx.drawImage(offscreenCanvas,this.playerX - this.game.camera.cameraX - this.scale * this.height / 2, this.playerY - this.game.camera.cameraY - this.scale * this.height / 2);
 
     }
 
@@ -184,13 +216,23 @@ class Steve {
             /*
             If the player pressed key, we will call animator to animate the movement of a player.
             */
-            this.animations.drawFrameAngle(this.game.clockTick, ctx, this.screenX, this.screenY, this.scale,angle);
+           if(this.run == true){
+            this.runningAnimations.drawFrameAngle(this.game.clockTick, ctx, this.screenX, this.screenY, this.scale,angle);
+           } else {
+            this.walkingAnimations.drawFrameAngle(this.game.clockTick, ctx, this.screenX, this.screenY, this.scale,angle);
+           }
+           
         } else {
             /*
             If the player is not moving, we will draw the image by calling drawAngle method.
             */
            this.drawAngle(ctx, degrees, this.scale);
-           this.animations.elapsedTime = 0;
+           if(this.run = true){
+            this.runningAnimations.elapsedTime = 0;
+           } else {
+            this.walkingAnimations.elapsedTime = 0;
+           }
+           
         }
 
         ctx.strokeStyle = "red";
