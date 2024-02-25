059c6368303d8a2514fc17e6d699c0e2ba4cf64a
karan <karandeepsangha95@gmail.com>
2024-02-24 19:17:38 -0800

    update

--- Steve.js
+++ Steve.js
@@ -25,7 +25,7 @@ class Steve {
 
         this.playerX = 0;
         this.playerY = 0;
-        this.playerZ = 0;
+        this.playerZ = 15;
         // this.playerX = playerX;
         // this.playerY = playerY;
          // this.playerZ = playerZ;
@@ -34,7 +34,7 @@ class Steve {
         this.screenY = this.game.ctx.canvas.height / 2;
 
         this.playerRunSpeed = 800;
-        this.playerWalkSpeed = 1;
+        this.playerWalkSpeed = 20;
 
 
         this.win = false;
