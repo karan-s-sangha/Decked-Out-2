61b35db804896bc4fc7d952f2c77b7a5ea1c29b2
karan <karandeepsangha95@gmail.com>
2024-02-28 09:06:21 -0800

    almost completed static art

--- Camera.js
+++ Camera.js
@@ -39,7 +39,7 @@ class Camera {
 
         this.blocksMap = {}; // Use an object as a hash map to store block data as objects
         this.layerCount = 37; // Set the number of layers you want to read
-        this.sizeFactor = 5;
+        this.sizeFactor = 2;
         let image = ASSET_MANAGER.cache["./Art/resources/tnt.png"];
         this.imageWidth = image.width;
         this.imageHeight = image.height;
