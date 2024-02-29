b99b436863e9cbb17b7425f9aa7e971cab270204
Khin_Win <kwin2@uw.edu>
2024-02-29 15:26:50 -0800

    ravagner

--- Ravager.js
+++ Ravager.js
@@ -65,14 +65,14 @@ class Ravager {
         let blockWidth = this.game.camera.imageWidth * this.game.camera.sizeFactor;
         let blockHeight = this.game.camera.imageHeight * this.game.camera.sizeFactor;
 
-        let isoX = (this.ravagerX - this.ravagerY) * blockWidth / 2 - this.game.camera.isoCameraX ;
-        let isoY = (this.ravagerX + this.ravagerY) * blockHeight / 4 - (this.ravagerZ - 2.5
-             - this.steve.playerZ) * blockWidth / 2 - this.game.camera.isoCameraY ;
+       /* let isoX = (this.ravagerX - this.ravagerY) * blockWidth / 2 - this.game.camera.isoCameraX - (blockWidth /2);
+        let isoY = ((this.ravagerX + this.ravagerY) * blockHeight / 4 )- (this.ravagerZ - 2.5
+             - this.steve.playerZ) * blockWidth / 2 - this.game.camera.isoCameraY ;*/
 
 
-       // let isoX = ((block.x - block.y) * imageWidth * sizeFactor / 2) - isoCameraX - (imageWidth * sizeFactor) / 2;
-       // let isoY = ((block.x + block.y) * imageHeight * sizeFactor / 4) - (block.z - playerZ) * 
-       //imageHeight * sizeFactor / 2 - isoCameraY + (imageHeight * sizeFactor) / 2;
+        let isoX = (this.ravagerX - this.ravagerY) * blockWidth / 2 - this.game.camera.isoCameraX - (blockWidth /2);
+        let isoY = ((this.ravagerX + this.ravagerY) * blockHeight / 4)- (this.ravagerZ 
+                 - this.steve.playerZ) * blockWidth/2 - this.game.camera.isoCameraY + blockHeight/2;
     
         // Draw entity
         let angle =  0; 
